aa1760b8650b31837fb5a77b73697be72298cf10
kcchik <kevin.c.chik@gmail.com>
2022-07-15 03:01:42 -0400

    make username smaller

--- api/models/user.py
+++ api/models/user.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class User(models.Model):
-    username = models.CharField(max_length=64)
+    username = models.CharField(max_length=16)
     created = models.DateTimeField(auto_now_add=True)
