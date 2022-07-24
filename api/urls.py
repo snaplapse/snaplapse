fe2214455dc25780ea1a5349443f4bfb8843a190
Daniel Kim <daniel.k.0710@gmail.com>
2022-07-23 23:27:58 -0400

    the rest of the models

--- api/urls.py
+++ api/urls.py
@@ -8,6 +8,16 @@ urlpatterns = [
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
     re_path('^users/(?P<username>.+)/$', views.UserDetailByName.as_view()),
+    path('photos/', views.PhotoList.as_view()),
+    path('photos/<int:pk>/', views.PhotoDetail.as_view()),
+    path('locations/', views.LocationList.as_view()),
+    path('locations/<int:pk>/', views.LocationDetail.as_view()),
+    path('likes/', views.LikeList.as_view()),
+    path('likes/<int:pk>/', views.LikeDetail.as_view()),
+    path('categories/', views.CategoryList.as_view()),
+    path('categories/<int:pk>/', views.CategoryDetail.as_view()),
+    path('tags/', views.TagList.as_view()),
+    path('tags/<int:pk>/', views.TagDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
