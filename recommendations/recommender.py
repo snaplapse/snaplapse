8767988d25efc3376f0beb21e7bf59bc23f1f324
Raymond Xia <ray.xia99@gmail.com>
2023-02-09 01:47:32 +0000

    Changed recommender to use scikit-learn Knn algorithm with cosine distance

--- recommendations/recommender.py
+++ recommendations/recommender.py
@@ -1,5 +1,8 @@
 from ast import increment_lineno
 from turtle import pd
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn import preprocessing
+from sklearn.neighbors import NearestNeighbors
 
 import pandas as pd
 pd.options.mode.chained_assignment = None  # default='warn'
@@ -80,6 +83,12 @@ print (locations)
 print ()
 
 def main():
+    #todo: break into functions
+    # load_data()
+    # process_data()
+    # make_recommmendations()
+
+
     # First let's make a copy of the locations
     locations_with_categories = locations.copy(deep=True)
 
@@ -141,53 +150,111 @@ def main():
     locations_with_proximities.drop(['locationId','location','category'], axis=1, inplace=True)
     locations_with_categories.drop(['locationId','location','category','proximity'], axis=1, inplace=True)
 
+
     # Viewing changes.
-    # print("Location-category matrix:")
-    # print(locations_with_categories.head())
+    print("Location-category matrix:")
+    print(locations_with_categories)
+    print()
 
     locationsDot = locations_with_categories.dot(user_profile)
-    print("Location-category scores:")
-    print(locationsDot)
-    print()
-    print("Location proximities:")
-    print(locations_with_proximities)
-    print()
+    # print("Location-category scores:")
+    # print(locationsDot)
+    # print()
+    # print("Location proximities:")
+    # print(locations_with_proximities)
+    # print()
+    # print(user_profile.values.reshape(1, -1))
+    user_profile_reshaped = user_profile.values.reshape(1, -1)
+    # technically we don't need to normalize since only the angle matters in cosine similarity
+    # user_profile_normalized = (user_profile_reshaped-user_profile_reshaped.min())/(user_profile_reshaped.max()-user_profile_reshaped.min())
 
-    # Multiply the categories by the weights and then take the weighted average.
-    recommendation_table_df = locationsDot / user_profile.sum()
-    print("Cosine similarities:")
-    print(recommendation_table_df)
-    print()
+    # print(user_profile_normalized)
 
-    for i in range(0,10) :
-        recommendation_table_df[i+1] = recommendation_table_df[i+1] * 1/(1+(float)(locations_with_proximities.iloc[i].iloc[0]))
+    model = NearestNeighbors()
+    model.set_params(**{
+        'n_neighbors': 10,
+        'algorithm': 'brute',
+        'metric': 'cosine',
+        'n_jobs': -1})
+    model.fit(locations_with_categories.values)
+    # print(model.effective_metric_)
+    distances, indices = model.kneighbors(user_profile_reshaped, n_neighbors=10)
 
-    # Let's view the recommendation table
-    # print(recommendation_table_df.head())
+    # print("Knn neighbour distance, indices")
+    # print(distances, indices)
+    # print()
 
-    # Let's sort values from great to small
-    recommendation_table_df.sort_values(ascending=False, inplace=True)
+    raw_recommends = \
+        sorted(
+            list(
+                zip(
+                    indices.squeeze().tolist(),
+                    distances.squeeze().tolist()
+                )
+            ),
+            key=lambda x: x[1]
+        )
 
-    #Just a peek at the values
-    print("Recommended locations by highest affinity: cosine similarity / (1+proximity)")
-    print(recommendation_table_df.head(20))
-    print()
+    # print(raw_recommends)
 
-    # first we make a copy of the original locations dataframe
-    copy = locations.copy(deep=True)
+    hashmap = {
+        location: i for i, location in 
+        enumerate(list(locations.set_index('locationId').loc[locations_with_categories.index].location)) # noqa
+    }
+    # print(hashmap)
 
-    # Then we set its index to locationId
-    copy = copy.set_index('locationId', drop=True)
+    reverse_hashmap = {v: k for k, v in hashmap.items()}
+    print('Recommendations')
+    for i, (idx, dist) in enumerate(raw_recommends):
+        print('{0}: {1}, with cosine distance '
+                'of {2}'.format(i+1, reverse_hashmap[idx], dist))
 
-    # Next we enlist the top 20 recommended locationIds we defined above
-    top_20_index = recommendation_table_df.index[:20].tolist()
 
-    # finally we slice these indices from the copied locations df and save in a variable
-    recommended_locations = copy.loc[top_20_index, :]
+    # Old manual cosine calculations
 
-    # Now we can display the top 20 locations in descending order of preference
-    print("Top 20 recommended locations:")
-    print(recommended_locations)
+    # # Multiply the categories by the weights and then take the weighted average.
+    # recommendation_table_df = locationsDot / user_profile.sum()
+    # print("Cosine similarities:")
+    # # print(recommendation_table_df)
+    # location_scores = cosine_similarity(user_profile_reshaped, locations_with_categories)
+    # print(location_scores)
+    # # print(cosine_similarity([[1, 0, -1]], [[-1,-1, 0], [1,1, 0]]))
+    # print()
+
+    # # for i in range(0,10) :
+    # #     recommendation_table_df[i+1] = recommendation_table_df[i+1] * 1/(1+(float)(locations_with_proximities.iloc[i].iloc[0]))
+    # location_scores = location_scores[0]
+    # for i in range(location_scores.size):
+    #     location_scores[i] = location_scores[i] /(1+(float)(locations_with_proximities.iloc[i].iloc[0]))
+
+    # # Let's view the recommendation table
+    # # print(recommendation_table_df.head())
+
+    # # Let's sort values from great to small
+    # # recommendation_table_df.sort_values(ascending=False, inplace=True)
+    # location_scores[::-1].sort()
+
+    # #Just a peek at the values
+    # print("Recommended locations by highest affinity: cosine similarity / (1+proximity)")
+    # # print(recommendation_table_df.head(20))
+    # print(location_scores)
+    # print()
+
+    # # first we make a copy of the original locations dataframe
+    # copy = locations.copy(deep=True)
+
+    # # Then we set its index to locationId
+    # copy = copy.set_index('locationId', drop=True)
+
+    # # Next we enlist the top 20 recommended locationIds we defined above
+    # top_20_index = location_scores[0:20]
+
+    # # finally we slice these indices from the copied locations df and save in a variable
+    # recommended_locations = copy.loc[top_20_index, :]
+
+    # # Now we can display the top 20 locations in descending order of preference
+    # print("Top 20 recommended locations:")
+    # print(recommended_locations)
 
 if __name__ == "__main__" :
       main()
