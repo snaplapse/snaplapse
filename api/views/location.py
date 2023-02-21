577aa85f57358a095ddb8e5d6f7309ff5f70ad80
Raymond Xia <ray.xia99@gmail.com>
2023-02-21 01:49:39 +0000

    Created nearbyLocations route

--- api/views/location.py
+++ api/views/location.py
@@ -1,7 +1,9 @@
 from rest_framework import generics
+from rest_framework.exceptions import ValidationError
 
 from ..models import Location
 from ..serializers import LocationSerializer
+import math
 
 class LocationList(generics.ListCreateAPIView):
     queryset = Location.objects.all()
@@ -10,3 +12,29 @@ class LocationList(generics.ListCreateAPIView):
 class LocationDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
+
+class NearbyLocations(generics.ListAPIView):
+    serializer_class = LocationSerializer
+    
+    def get_queryset(self):
+        params = self.request.query_params
+        
+        try:
+            latitude = float(params.get('coordinates').split(',')[0])
+            longitude = float(params.get('coordinates').split(',')[1])
+            radius = float(params.get('radius'))
+
+            lat1 = latitude - (radius / 6378000) * (180 / math.pi)
+            lat2 = latitude + (radius / 6378000) * (180 / math.pi)
+            lng1 = longitude - (radius / 6378000) * (180 / math.pi) / math.cos(latitude * math.pi/180)
+            lng2 = longitude + (radius / 6378000) * (180 / math.pi) / math.cos(latitude * math.pi/180)
+            queryset = Location.objects.filter(
+                latitude__gte = lat1, 
+                latitude__lte = lat2,
+                longitude__gte = lng1,
+                longitude__lte = lng2
+            ).order_by('id')
+
+            return queryset
+        except:
+            raise ValidationError("Invalid request.")
