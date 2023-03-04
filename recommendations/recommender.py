5c4cf62dfe0464810831fb6e0732111cd0702afc
Raymond Xia <ray.xia99@gmail.com>
2023-03-04 13:51:59 -0500

    Fixed recommender for new google_id column

--- recommendations/recommender.py
+++ recommendations/recommender.py
@@ -15,8 +15,8 @@ def load_data(host, latitude, longitude, radius):
     get_locations = json.loads(requests.get(nearby_locations_url, timeout=10).content.decode("utf-8"))
     locations_list = []
     for location in get_locations['results']:
-        locations_list.append((location['id'], location['name'], location['latitude'], location['longitude'], location['categories']))
-    locations = pd.DataFrame(locations_list, columns = ['location_id', 'name', 'latitude', 'longitude', 'categories']).sort_values(by=['location_id'])
+        locations_list.append((location['id'], location['name'], location['latitude'], location['longitude'], location['categories'], location['google_id']))
+    locations = pd.DataFrame(locations_list, columns = ['location_id', 'name', 'latitude', 'longitude', 'categories', 'google_id']).sort_values(by=['location_id'])
     locations.set_index('location_id', drop=False, inplace=True)
     # print(locations)
 
@@ -47,12 +47,12 @@ def generate_affinity_recommendations(user_id, likes, locations, num_recs):
     # Filter to only the relevant categories by selecting locations that exist in both user_likes and locations_with_categories
     user_categories = locations_with_categories[locations_with_categories.location_id.isin(user_likes.location_id)]
     # Drop redundant columns
-    user_categories.drop(['location_id','name','latitude', 'longitude','categories'], axis=1, inplace=True)
+    user_categories.drop(['location_id','name','latitude', 'longitude','categories','google_id'], axis=1, inplace=True)
     # Take dot product of transpose of user_categories by user_likes column to get user category affinities
     user_affinities = user_categories.T.dot(user_likes.likes)
 
     # Delete unnecessary columns
-    locations_with_categories.drop(['location_id','name','categories','latitude','longitude'], axis=1, inplace=True)
+    locations_with_categories.drop(['location_id','name','latitude','longitude','categories','google_id'], axis=1, inplace=True)
 
     user_affinities = user_affinities.values.reshape(1, -1)
 
