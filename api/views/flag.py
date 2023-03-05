96e756aa27ce91f41e540e3a97d1e1586a12131a
kcchik <kevin.c.chik@gmail.com>
2023-03-04 20:07:35 -0500

    filter likes/flags

--- api/views/flag.py
+++ api/views/flag.py
@@ -8,6 +8,16 @@ class FlagList(generics.ListCreateAPIView):
     queryset = Flag.objects.all()
     serializer_class = FlagSerializer
 
+    def get_queryset(self):
+        user = self.request.query_params.get('user')
+        photo = self.request.query_params.get('photo')
+        flags = Flag.objects.all()
+        if user is not None:
+            flags = flags.filter(user=user)
+        if photo is not None:
+            flags = flags.filter(photo=photo)
+        return flags
+
 
 class FlagDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Flag.objects.all()
